a34bbb9856d28e4f2cc49bbf54df532ab1e88c1c
Arindam Kalra <arindamkalra@gmail.com>
2026-05-31 18:24:19 +0530

    Add PWA support - manifest, service worker, icons

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = 'interests-v1';
+const ASSETS = ['/interests-tracker/', '/interests-tracker/index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  // network first, fall back to cache
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
